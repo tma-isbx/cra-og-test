5293bd0dc678737b931772ac3d837a5f11124ac3
Tienshiao Ma <tienshiao@tienshiao.org>
2022-08-25 01:38:17 -0700

    Add POC

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+
+function Home() {
+  return (
+    <>
+      <Helmet>
+        <title>Home</title>
+        <meta property="og:title" content="This is the home og:title." />
+        <meta property="og:description" content="This is the home og:description." />
+      </Helmet>
+      <div>Home</div>
+    </>
+  );
+}
+function Page1() {
+  return (
+    <>
+      <Helmet>
+        <title>Page 1</title>
+        <meta property="og:title" content="This is the Page 1 title." />
+        <meta property="og:description" content="This is the Page 1 og:description." />
+      </Helmet>
+      <div>Page1</div>
+    </>
+  );
+}
+
+function Page2() {
+  return (
+    <>
+      <Helmet>
+        <title>Page 2</title>
+        <meta property="og:title" content="This is the Page 2 og:title." />
+        <meta property="og:description" content="This is the Page 2 og:description." />
+      </Helmet>
+      <div>Page2</div>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
